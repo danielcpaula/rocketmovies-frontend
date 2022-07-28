87b13e8feb21bed4f493fd1990d84dd2c02884c5
Daniel do Casal de Paula <daniel.c.paula@hotmail.com>
2022-07-28 10:54:11 -0300

    Frontend rocketmovie project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# rocketmovies-frontend
-Projeto do desafio rocketmovies
+# Rocket-movies
+<img height="300px" src="./src/assets/Screenshot.png"> 
+
+Página web criada em React a partir de layout no figma.
+Desafio do explorer da Rockeseat. 
+
+
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import { Container, Container2 } from "./styles";
+
+export function Button({ title, loading, ...rest }) {
+  return (
+    <Container type="button" disabled={loading} {...rest}>
+      {loading ? "Carregando..." : title}
+    </Container>
+  );
+}
+
+export function ButtonDark({ title }) {
+  return <Container2 type="button">{title}</Container2>;
+}
